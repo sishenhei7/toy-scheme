9d5a4278b6a783cf48c714a5fae419f719f0c546
sishenhei7 <982505136@qq.com>
2023-01-28 22:47:17 +0800

    refactor: lint and add token

---
+++ src/scheme/parser.ts
@@ -0,0 +1,28 @@
+enum tokenKind {
+  WhiteSpace,
+  Boolean,
+  LParen,
+  RParen,
+  Symbol,
+  Number,
+  String,
+  Quote,
+  Comment,
+  EOF,
+}
+
+export class Token {
+  constructor(public kind: tokenKind, public value: string){}
+}
+
+export class Parser {
+  private readonly regexSymbol = /^[^\s()',]+/;
+  private readonly regexNumber = /^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/;
+  private readonly regexString = /^"([^\\"]+|\\.)*"/;
+  private readonly regexWhiteSpace = /^\s*/;
+  private readonly regexBoolean = /^#t|^#f/;
+  private readonly regexComment = /^;.*/;
+
+  private tokens: Token[] = [];
+  private cursor = 0;
+}
