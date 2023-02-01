75dc1caab58e1b255badbe94aa99bc77b6f74447
yangzhou <zhou.yang@klook.com>
2023-02-01 11:58:01 +0800

    refactor: refactor parse

--- src/scheme/index.ts
+++ src/scheme/index.ts
@@ -1,1 +1,18 @@
-export default {};
+import { Parser } from "./parser";
+
+const program = `
+  ; Factorial
+
+  (define (factorial x)
+      (cond
+          ((= x 1) 1)
+          (else (* x (factorial (- x 1))))))
+
+  (display (factorial 5))
+
+`
+
+export function test() {
+  const parser = new Parser()
+  console.log(parser.parse(program))
+}
--- src/scheme/parser.ts
+++ src/scheme/parser.ts
@@ -1,32 +1,72 @@
-enum tokenKind {
-  WhiteSpace,
-  Boolean,
+/**
+ * parser 接受一个 st 字符串，然后返回一个 ast 结构
+ * 注意：为了更好地扩展性，这里不涉及任何 let、define 等语法，这些语法在后面处理
+ */
+
+enum TokenType {
+  Quote,
   LParen,
   RParen,
   Symbol,
   Number,
   String,
-  Quote,
+  WhiteSpace,
+  Boolean,
   Comment,
   EOF,
 }
 
+const regexList: [TokenType, RegExp][] = [
+  [TokenType.Quote, /^'/],
+  [TokenType.LParen, /^\(/],
+  [TokenType.RParen, /^\)/],
+  [TokenType.Symbol, /^[^\s()',]+/],
+  [TokenType.Number, /^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/],
+  [TokenType.String, /^"([^\\"]+|\\.)*"/],
+  [TokenType.WhiteSpace, /^\s*/],
+  [TokenType.Boolean, /^#t|^#f/],
+  [TokenType.Comment, /^;.*/]
+]
+
 export class Token {
-  constructor(public kind: tokenKind, public value: string){}
+  constructor(
+    private type: TokenType,
+    private start: number,
+    private end: number,
+  ) { }
 }
 
-export class Parser {
-  private readonly regexSymbol = /^[^\s()',]+/;
-  private readonly regexNumber = /^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/;
-  private readonly regexString = /^"([^\\"]+|\\.)*"/;
-  private readonly regexWhiteSpace = /^\s*/;
-  private readonly regexBoolean = /^#t|^#f/;
-  private readonly regexComment = /^;.*/;
+export function parse(st: string) {
+  const stack = []
+  let index = 0
+  return parseExpression()
 
-  private tokens: Token[] = [];
-  private cursor = 0;
+  function getNextToken() {
+    for (const [token, reg] of regexList) {
+      const matched = reg.exec(st)
 
-  public parse(sentence: string) {
+      if (matched) {
+        return {
+          token,
+          len: matched[0].length
+        }
+      }
+    }
 
+    return {
+      token: TokenType.EOF,
+      len: 0
+    }
+  }
+
+  function advance(n: number): void {
+    index += n
+    st = st.substring(n)
+  }
+
+  function parseExpression() {
+    const { token, len } = getNextToken()
+    stack.push(new Token(token, index, index + len))
+    advance(len)
   }
 }
