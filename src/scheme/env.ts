66dfd8f8aa9365a2f101c0e73ae47f35cff08916
yangzhou <zhou.yang@klook.com>
2023-02-06 16:17:13 +0800

    refactor: refactor env

--- src/scheme/env.ts
+++ src/scheme/env.ts
@@ -1,1 +1,40 @@
-export class Env {}
+export class StackFrame {
+
+}
+
+export class Env {
+  private obj: Map<string, any> = new Map()
+
+  constructor(private parentEnv?: Env | null = null, private stackFrame?: StackFrame | null = null) {}
+
+  // TODO: add return type
+  public get(key: string): any {
+    if (this.obj.has(key)) {
+      return this.obj.get(key)
+    }
+
+    if (!this.parentEnv) {
+      throw Error('${key} is not defined!')
+    }
+
+    return this.parentEnv.get(key)
+  }
+
+  // TODO: add type
+  public set(key: string, value: any): void {
+    if (this.obj.has(key)) {
+      this.obj.set(key, value)
+    }
+
+    if (!this.parentEnv) {
+      throw Error('${key} is not defined!')
+    }
+
+    this.parentEnv.set(key, value)
+  }
+
+  // TODO: add type
+  public define(key: string, value: any): void {
+    this.obj.set(key, value)
+  }
+}
--- src/scheme/evaluator/index.ts
+++ src/scheme/evaluator/index.ts
@@ -1,1 +1,9 @@
-export class Evaluator {}
+import type { INode } from '../node'
+import type { Env } from '../env'
+import type { Cont } from './continuation'
+
+export class BaseEvaluator {
+  public evaluate(node: INode, env: Env, cont: Cont) {
+
+  }
+}
