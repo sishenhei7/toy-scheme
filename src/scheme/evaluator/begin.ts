86fb7483ffd13df18e5b38c173768b96c420b429
sishenhei7 <982505136@qq.com>
2023-02-11 11:37:41 +0800

    refactor: implement cond

--- src/scheme/evaluator/begin.ts
+++ src/scheme/evaluator/begin.ts
@@ -1,6 +1,7 @@
-import type { BaseData } from '../parser/data'
+import type { BaseData, SchemeExp } from '../parser/data'
 import type { Env } from '../env'
 import type { IEvaluator, Evaluator, Cont } from './index'
+import { assert } from '../utils'
 
 /**
  * 语法：
@@ -16,13 +17,17 @@ export class BeginEvaluator implements IEvaluator {
     return tag === 'begin'
   }
 
-  public evaluate(node: BaseData, env: Env, cont: Cont): BaseData {
-    return node
-  }
+  public evaluate(node: SchemeExp, env: Env, cont: Cont): BaseData {
+    let currentNode = node.body
+    assert(currentNode, 'Syntax error: begin followed no clause!')
 
-  private getCont(node: BaseData, env: Env, cont: Cont) {
-    return (val: BaseData) => {
-
+    let res = null
+    while (currentNode) {
+      res = this.evaluator.evaluate(currentNode, env, cont)
+      currentNode = currentNode.next
     }
+
+    assert(res, 'Syntax error: begin clause evaluate error!')
+    return res
   }
 }
--- src/scheme/evaluator/cond.ts
+++ src/scheme/evaluator/cond.ts
@@ -1,6 +1,7 @@
-import type { BaseData } from '../parser/data'
+import { SchemeBoolean, SchemeSym, type BaseData, type SchemeExp } from '../parser/data'
 import type { Env } from '../env'
 import type { IEvaluator, Evaluator, Cont } from './index'
+import { assert } from '../utils'
 
 /**
  * 语法：
@@ -17,7 +18,38 @@ export class CondEvaluator implements IEvaluator {
     return tag === 'cond'
   }
 
-  public evaluate(node: BaseData, env: Env, cont: Cont): BaseData {
+  public evaluate(node: SchemeExp, env: Env, cont: Cont): BaseData {
+    let currentNode = node.body
+    let res = null
+
+    while (currentNode) {
+      const predict = this.getPredicate(currentNode)
+
+      if (SchemeSym.matches(predict) && predict.tag === 'else') {
+        res = this.evaluator.evaluate(this.getValue(currentNode), env, cont)
+        break
+      }
+
+      const predictValue = this.evaluator.evaluate(predict, env, cont)
+      if (SchemeBoolean.isTrue(predictValue)) {
+        res = this.evaluator.evaluate(this.getValue(currentNode), env, cont)
+        break
+      }
+
+      currentNode = currentNode.next
+    }
+
+    assert(res, 'Syntax error: cond clause evaluate error!')
+    return res
+  }
+
+  private getPredicate(node: BaseData | null): BaseData {
+    assert(node, 'Syntax error: cond clause need predicate clause!')
     return node
+  }
+
+  private getValue(node: BaseData | null): BaseData {
+    assert(node?.next, 'Syntax error: cond clause need value clause!')
+    return node.next
   }
 }
